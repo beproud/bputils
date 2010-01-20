d330a8c385972623056c13e9e0e57ccd31862d83
Ian Lewis <IanMLewis@gmail.com>
2010-01-20 15:48:02 +0900

    cos, sin, exp, pi を追加。force_decimalのfloat変換を改善

--- bputils/mathutils/decimalutils.py
+++ bputils/mathutils/decimalutils.py
@@ -1,22 +1,41 @@
 #:coding=utf8:
 
-from decimal import Decimal, getcontext, setcontext, InvalidOperation
+from decimal import (
+    Decimal,
+    Context,
+    getcontext,
+    setcontext,
+    InvalidOperation,
+    Inexact,
+)
 
 #def force_decimal(obj,  
 __all__ = (
     'force_decimal',
+    'pi',
+    'exp',
+    'cos',
+    'sin',
     'log',
     'ln',
 )
 
-def force_decimal(d, default=None, numbers_only=False):
+def force_decimal(d, precision_loss=True, numbers_only=False):
     """
     Coerces a value to a Decimal object. 
 
-    float objects are first converted to strings before being
-    converted to Decimal objecs. This can cause a loss of
-    precision as the float objects are rounded. The 'str'
-    method is used since it provides better precision than "%f" % float
+    If precision_loss is True then float objects are first converted
+    to strings before being converted to Decimal objects. This can cause
+    a loss of precision as the float objects are rounded. The 'str'
+    method is used since it provides better precision than "%f" % float.
+
+    If you wish to preserve as much information as possible you can call
+    the method with precision_loss=False and method will be used that
+    can convert the float without loosing information. This can, however,
+    add extra information due to issues with binary floating point.
+    
+    See: Is there a way to convert a regular float to a Decimal?
+    http://docs.python.org/library/decimal.html#decimal-faq
 
     If numbers_only is True, don't convert (some) non-number-like objects.
     Strings that can be converted to Decimals are still converted.
@@ -27,7 +46,7 @@ def force_decimal(d, default=None, numbers_only=False):
         elif isinstance(d, (int, long)):
             return Decimal(d)
         elif isinstance(d, float):
-            return Decimal(str(d))
+            return Decimal(str(d)) if precision_loss else float_to_decimal(d)
         elif isinstance(d, (basestring, unicode)):
             return Decimal(d)
     except InvalidOperation:
@@ -38,10 +57,120 @@ def force_decimal(d, default=None, numbers_only=False):
     else:
         raise ValueError("Cannot convert object to Decimal: %r" % d)  
 
+def float_to_decimal(f):
+    """
+    Convert a floating point number to a Decimal with no loss of information
+    """
+    n, d = f.as_integer_ratio()
+    numerator, denominator = Decimal(n), Decimal(d)
+    ctx = Context(prec=60)
+    result = ctx.divide(numerator, denominator)
+    while ctx.flags[Inexact]:
+        ctx.flags[Inexact] = False
+        ctx.prec *= 2
+        result = ctx.divide(numerator, denominator)
+    return +result
+
+def pi():
+    """
+    Compute Pi to the current precision.
+    Taken from: http://docs.python.org/library/decimal.html#decimal-recipes
+
+    >>> print pi()
+    3.141592653589793238462643383
+    """
+    getcontext().prec += 2  # extra digits for intermediate steps
+    three = Decimal(3)      # substitute "three=3.0" for regular floats
+    lasts, t, s, n, na, d, da = 0, three, 3, 1, 0, 0, 24
+    while s != lasts:
+        lasts = s
+        n, na = n+na, na+8
+        d, da = d+da, da+32
+        t = (t * n) / d
+        s += t
+    getcontext().prec -= 2
+    return +s               # unary plus applies the new precision
+
+def exp(x):
+    """
+    Return e raised to the power of x.  Result type matches input type.
+    Taken from: http://docs.python.org/library/decimal.html#decimal-recipes
+
+    >>> print exp(Decimal(1))
+    2.718281828459045235360287471
+    >>> print exp(Decimal(2))
+    7.389056098930650227230427461
+    >>> print exp(2.0)
+    7.38905609893
+    >>> print exp(2+0j)
+    (7.38905609893+0j)
+
+    """
+    getcontext().prec += 2
+    i, lasts, s, fact, num = 0, 0, 1, 1, 1
+    while s != lasts:
+        lasts = s
+        i += 1
+        fact *= i
+        num *= x
+        s += num / fact
+    getcontext().prec -= 2
+    return +s
+
+def cos(x):
+    """
+    Return the cosine of x as measured in radians.
+    Taken from: http://docs.python.org/library/decimal.html#decimal-recipes
+
+    >>> print cos(Decimal('0.5'))
+    0.8775825618903727161162815826
+    >>> print cos(0.5)
+    0.87758256189
+    >>> print cos(0.5+0j)
+    (0.87758256189+0j)
+    """
+    getcontext().prec += 2
+    i, lasts, s, fact, num, sign = 0, 0, 1, 1, 1, 1
+    while s != lasts:
+        lasts = s
+        i += 2
+        fact *= i * (i-1)
+        num *= x * x
+        sign *= -1
+        s += num / fact * sign
+    getcontext().prec -= 2
+    return +s
+
+def sin(x):
+    """
+    Return the sine of x as measured in radians.
+    Taken from: http://docs.python.org/library/decimal.html#decimal-recipes
+
+    >>> print sin(Decimal('0.5'))
+    0.4794255386042030002732879352
+    >>> print sin(0.5)
+    0.479425538604
+    >>> print sin(0.5+0j)
+    (0.479425538604+0j)
+    """
+    getcontext().prec += 2
+    i, lasts, s, fact, num, sign = 1, 0, x, 1, x, 1
+    while s != lasts:
+        lasts = s
+        i += 2
+        fact *= i * (i-1)
+        num *= x * x
+        sign *= -1
+        s += num / fact * sign
+    getcontext().prec -= 2
+    return +s
+
 def log(self, base=10, context=None):
     """
     Returns a log of arbitrary precision using the method
     described here: http://www.programmish.com/?p=25
+
+    Input must be a decimal
     """
     old_context = None
     if context is not None:
@@ -83,8 +212,12 @@ def log(self, base=10, context=None):
     return integer_part + decimal_frac
 
 def ln(self, context=None):
+    """
+    Returns the natural log of the given Decimal
+
+    Input must be a decimal
+    """
     if hasattr(self, "ln"):
         return self.ln(context)
     else:
-        import math
-        return log(self, base=force_decimal(math.e)) 
+        return log(self, base=exp(Decimal("1.0"))) 
