d6c85892b7aad495d6601081302cd2bc226929a5
Ian Lewis <IanMLewis@gmail.com>
2010-02-21 00:12:19 +0900

    wordテスト追加

--- bputils/tests/word_tests.py
+++ bputils/tests/word_tests.py
@@ -31,6 +31,22 @@ class TokenizerTestCase(TestCase):
             for feature in TEST_FEATURE_MAP:
                 self.assertTrue(feature in token.features)
 
+    def test_word_classes(self):
+        tokenizer = Tokenizer(word_classes=["名詞"])
+        tokens = tokenizer.tokenize(u"これはテスト文章です")
+        token_list = list(tokens)
+        self.assertEquals(len(token_list), 3)
+        self.assertEquals(token_list[0], u"これ")
+        self.assertEquals(token_list[1], u"テスト")
+        self.assertEquals(token_list[2], u"文章")
+
+    def test_min_word_length(self):
+        tokenizer = Tokenizer(min_word_length=3)
+        tokens = tokenizer.tokenize(u"これはテスト文章です")
+        token_list = list(tokens)
+        self.assertEquals(len(token_list), 1)
+        self.assertEquals(token_list[0], u"テスト")
+
     def test_callback(self):
         def cb(word, features):
             self.assertTrue(isinstance(features, dict))
@@ -44,3 +60,10 @@ class TokenizerTestCase(TestCase):
         tokens = tokenizer.tokenize(u"これはテスト文章です")
         self.assertEquals(len(list(tokens)), 5)
         self.assertEquals(cb.count, 5)
+    
+    def test_callback_none(self):
+        def cb(word, features):
+            pass
+        tokenizer = Tokenizer(token_callback=cb)
+        tokens = tokenizer.tokenize(u"これはテスト文章です")
+        self.assertEquals(len(list(tokens)), 0)
