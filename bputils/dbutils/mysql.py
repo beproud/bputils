e2e986c9bfb19b66070ed2eee8f24d4dca0e33cb
Ian Lewis <IanMLewis@gmail.com>
2010-08-05 20:42:27 +0900

    イテレーターの整理とテスト追加
    --HG--
    branch : t61_low_level_cursor

--- bputils/dbutils/mysql.py
+++ bputils/dbutils/mysql.py
@@ -1,5 +1,9 @@
 #:coding=utf-8:
 
+TUPLE=0
+DICT=1
+DICT_WITH_TABLE=2
+
 def MySQLConnection(connect_info):
     ''' 単純にMySQLのコネクションを作って返します。 '''
     import MySQLdb
@@ -23,7 +27,8 @@ class MySQLCursor(object):
     """
 
     _next = None
-    def __init__(self, connection, rows=1, value_type=1):
+    _index = 0
+    def __init__(self, connection, rows=1, value_type=DICT):
         self.connection = connection
         self.rows = rows
         self.value_type = value_type
@@ -40,13 +45,18 @@ class MySQLCursor(object):
 
     def has_next(self):
         if self._next is None:
-            self.row = self.rs.fetch_row(self.rows, self.value_type)
+            if not self.row or self._index+1 > len(self.row):
+                self.row = list(self.rs.fetch_row(self.rows, self.value_type) or [])
+                self._index = 0
             self._next = bool(self.row)
         return self._next
 
     def next(self):
         if self.has_next():
             self._next = None
-            return self.row[0] if self.rows == 1 else self.row
+            return_val = self.row[self._index]
+            self._index += 1
+            self.has_next() # Check for next values
+            return return_val
         else:
             raise StopIteration  
