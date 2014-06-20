378e82fa4837d24a12a3be8ce5835301f590b80e
Ian Lewis <IanMLewis@gmail.com>
2014-06-20 10:58:23 +0900

    バージョンを0.34に更新

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
- 
-setup (
+
+setup(
     name='beproud.utils',
-    version='0.33',
+    version='0.34',
     description='Dependency free utilities',
     author='Ian Lewis',
     author_email='ianmlewis@beproud.jp',
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Programming Language :: Python',
-      'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
     namespace_packages=['beproud'],
